ffee62fea82b3183d348c34f57b8fcc85749e9b1
Micaela Estabillo <micaela.estabillo@gmail.com>
2015-11-29 07:50:17 -0500

    button click calls bellman-ford function but it doesn't return correct values. introducing bit manipulation solution meanwhile

--- client/arbitrage.js
+++ client/arbitrage.js
@@ -1,92 +1,30 @@
 if (Meteor.isClient) {
-  // refresh exchange rates
-  Rates.refreshRates();
-
+  Meteor.startup(function () {
+  });
+    
   Template.textinput.events({
-    "submit .textinput": function (event) {
-      // Prevent default browser form submit
-      event.preventDefault();
-
-      // Get value from form element
-      var amount = event.target.amount.value;
-      Session.setDefault('amount', amount);
-
-      // Clear form
-      // event.target.text.value = "";
-    }
   });
 
   Template.textinput.helpers({
   });
 
   Template.menu.events({
-    "onChange .menu": function (event) {
-      event.preventDefault();
-
-      var currencytemp = document.getElementById("select-currency");
-      var currency = currencytemp[currencytemp.selectedIndex].value;
-
-      Session.setDefault('currency', currency);
-    }
   });
 
-  //Template.menu.helpers({
-  //});
-
-  Template.arbitrate.events({
-    'click button': function () {
-      bellmanford(Session.get('amount'), Session.get('currency'));
-    }
+  Template.menu.helpers({
   });
 
   Template.arbitrate.helpers({
-    bellmanford: function (a,c) {
-      var NO_OF_CUR = 3;
-
-      var nodes = ["USD","CAD","EUR"];
-      var exchangeRates = [1,1.34,0.94
-                          ,0.75,1,0.71
-                          ,1.76,1.42,1];
-
-      var weight = [];
-      //weight = weight between edges
-      var i,j,k;
-      for (i=0;i<NO_OF_CUR*NO_OF_CUR;i++){
-        weight [i] = - Math.log(exchangeRates[i]);
-      }
-      //initialise distances
-      var dist = [];
-      var pred = [];
-      for (i = 0; i < NO_OF_CUR ; i++){
-        dist[i] = Infinity;
-        pred[i] = -1;
-      }
-      dist[0] = 0;
-
-      for ( k = 0; k < NO_OF_CUR ; k ++ ){
-        for ( i = 0; i < NO_OF_CUR; i ++ ){
-          for (j = 0 ; j < NO_OF_CUR; j++)
-            if (dist[i] + weight[i*NO_OF_CUR+ j] < dist[j]){
-              dist[j] = dist[i] + weight[i*NO_OF_CUR+j];
-              pred[j] = i;
-              window.alert(weight[i*NO_OF_CUR+j]);
-            }
-        }
-      }
-      window.alert("final answer" + weight[2*NO_OF_CUR+2]);
-      //checking for negative weight cycle
-      for (i = 0; i < NO_OF_CUR; i++) {
-        for (j = 0; j < NO_OF_CUR; j++) if (dist[i] + weight[i*NO_OF_CUR+j] < dist[j]) {
-          window.alert(j + " Is a part of negative cycle");
-        }
-        else {
-          window.alert ("No possibility for arbitrage");
-        }
-      }
+  });
+    
+  Template.arbitrate.events({
+    'click #arbitrate': function () {
+      //event.preventDefault();
+      amount = document.getElementById("textinput").value;
+      currency = document.getElementById("select-currency").value;
+      if (amount && currency)
+        Meteor.call('bellmanFord', amount, currency);
     }
-
-
-
   });
 
 }
--- server/server.js
+++ server/server.js
@@ -1,6 +1,87 @@
+
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+
+  Rates.config({
+    collectionName: 'rates',
+    autoRefresh: false
+  });
+
+  Meteor.methods({
+    'refreshRates': function () { 
+      Rates.refreshRates();
+    },
+
+    'bellmanFord': function (amount,currency) {
+      nodes = ["AUD", "BGN", "BRL", "CAD", "CHF", "CNY", "CZK", "DKK", "EUR", "GBP", "HKD", "HRK", "HUF", "IDR", "ILS", "INR", "JPY", "KRW", "LTL", "LVL", "MXN"]; //, "MYR", "NOK", "NZD", "PHP", "PLN", "RON", "RUB", "SEK", "SGD", "THB", "TRY", "USD", "ZAR"];
+      result1 = amount;
+      cur_amount = amount;
+      cur_currency = currency;
+      //cur_currency_index = nodes.indexOf(cur_currency);
+      max_amount = 0;
+      max_index = 0; //index of path
+
+//      nodes = ['AUD', 'BGN', 'BRL', 'CAD', 'CHF', 'CNY', 'CZK', 'DKK', 'EUR', 'GBP', 'HKD', 'HRK', 'HUF', 'IDR', 'ILS', 'INR', 'JPY', 'KRW', 'LTL', 'LVL', 'MXN', 'MYR', 'NOK', 'NZD', 'PHP', 'PLN', 'RON', 'RUB', 'SEK', 'SGD', 'THB', 'TRY', 'USD', 'ZAR'];
+      node_indices = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];//,21,22,23,24,25,26,27,28,29,30,31,32,33]; // nodes is 34 elements long
+      //remove root from nodes, but preserve nodes array
+     // othernodes = node_indices;
+     // var index = nodes.indexOf(currency);
+     // if (index > -1) othernodes.splice(index,1);
+
+
+/*
+     pwr = Combinatorics.power(node_indices);
+     pwr.forEach(function(a){
+       perm = Combinatorics.permutation(a);
+       perm.forEach(function(b){
+         console.log(b.toArray());
+       });
+     });
+
+      for (i = 2; i < 21; i++){
+        cmb = Combinatorics.combination(node_indices,i); //,3,4,5]); //,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]);
+        cmb.forEach(function(){
+
+        });
+      }
+      for (i=0; i<cmb.toArray().length; i ++){
+        j = 0;
+        do {
+          if (cmb.toArray().nth(i).indexOf(cur_currency_index) > -1){
+            j++;
+          }else{
+            if (nodes[j] != currency) {
+            Rates.convert(cur_amount, cur_currency, nodes[j], function (error, result) {
+              if (error) {
+                console.log('error',error);
+              } else {
+                console.log(result);
+              }
+              result1 = result;
+            });
+            }else if (j == cmb.toArray().nth(i).length){
+              Rates.convert(cur_amount, cur_currency, currency, function (error, result) {
+                if (error) {
+                  console.log('error',error);
+                } else {
+                  console.log(result);
+                }
+                result1 = result;
+              });
+            }
+            cur_amount = result1.exchangedAmount;
+            cur_currency = result1.to;
+            j ++;
+          }
+        }while(j < cmb.toArray().nth(i).length);
+        if (cur_amount > max_amount){
+          max_amount = cur_amount;
+          max_index = i;
+        }
+      }
+      
+*/
+      answer = 'best path is at ' + max_index + ' with ' + max_amount;
+      console.log(answer);
+    }
   });
 }
-
